171fc7fbd85d640d9d367e565b3285208dd5d539
a.martynov <a.martynov@qiwi.com>
2020-09-11 10:32:35 +0300

    Martynov test commit 1

--- skynet/src/Main.kt
+++ skynet/src/Main.kt
@@ -4,8 +4,9 @@ import robot.SpyMission
 //import robot.RobotFabrica.*
 
 fun main() {
+    println("Всем привет")
     val robot1T600 = RuFactory.makeRobot(RobotType.T_600, SpyMission())
-    val robot2T600 = RuFactory.makeRobot(RobotType.T_600, SpyMission())
+    //val robot2T600 = RuFactory.makeRobot(RobotType.T_600, SpyMission())
     val robot3T800 = UsaFactory.makeRobot(RobotType.T_800, KillHumanMission())
     val robot4T800 = UsaFactory.makeRobot(RobotType.T_800, KillHumanMission())
     robot4T800.awake()
